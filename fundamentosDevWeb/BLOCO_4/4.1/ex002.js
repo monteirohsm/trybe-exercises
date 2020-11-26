28d358984d62731f9c8a9c6d426c4c47d1844100
Matheus Monteiro <monteiromsh@gmail.com>
2020-11-25 23:47:58 -0300

    ex002 + ajuste ident

--- fundamentosDevWeb/BLOCO_4/4.1/ex002.js
+++ fundamentosDevWeb/BLOCO_4/4.1/ex002.js
@@ -1,14 +1,14 @@
 // Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
 
-function maiorNumero(a,b){
-    a = 50
-    b = 20
-    if (a > b){
-        console.log('O maior número é', + a)
-    } else if (b > a){
-        console.log('O maior número é', + b)
-    } else {
-        console.log('Error')
-    }
+function maiorNumero(a, b) {
+  a = 50;
+  b = 20;
+  if (a > b) {
+    console.log("O maior número é", +a);
+  } else if (b > a) {
+    console.log("O maior número é", +b);
+  } else {
+    console.log("Error");
+  }
 }
-maiorNumero()
+maiorNumero();
