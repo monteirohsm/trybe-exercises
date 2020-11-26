b1841ded17c1d01101706adfe25fce6d3904d4c5
Matheus Monteiro <monteiromsh@gmail.com>
2020-11-25 23:47:38 -0300

    ex001 + ajuste ident

--- fundamentosDevWeb/BLOCO_4/4.1/ex001.js
+++ fundamentosDevWeb/BLOCO_4/4.1/ex001.js
@@ -2,35 +2,35 @@
 
 // Adicao
 function adicao(a, b) {
-let soma = a + b;
-return soma;
+  let soma = a + b;
+  return soma;
 }
 console.log(adicao(2, 3));
 
 // Subtracao
 function subtracao(a, b) {
-let subtrair = a - b;
-return subtrair;
+  let subtrair = a - b;
+  return subtrair;
 }
 console.log(subtracao(15, 5));
 
 // Multiplicacao
 function multiplicacao(a, b) {
-let multiplicar = a * b;
-return multiplicar;
+  let multiplicar = a * b;
+  return multiplicar;
 }
 console.log(multiplicacao(5, 5));
 
 // Divisao
 function divisao(a, b) {
-let dividir = a / b;
-return dividir;
+  let dividir = a / b;
+  return dividir;
 }
 console.log(divisao(40, 2));
 
 // Modulo
 function restoDaDivisao(a, b) {
-let modulo = a % b;
-return modulo;
+  let modulo = a % b;
+  return modulo;
 }
 console.log(restoDaDivisao(9, 2));
