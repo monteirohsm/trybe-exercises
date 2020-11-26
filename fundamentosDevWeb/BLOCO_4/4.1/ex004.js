89f4b219354c7dbcf8912d443c7b3ab16720fcb4
Matheus Monteiro <monteiromsh@gmail.com>
2020-11-26 00:17:25 -0300

    ex004 concluido

---
+++ fundamentosDevWeb/BLOCO_4/4.1/ex004.js
@@ -0,0 +1,11 @@
+// Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+function positoNegativo (a){
+    a = -53
+    if (a > 0){
+        console.log('positive')
+    } else {
+        console.log('negative')
+    }
+}
+positoNegativo()
